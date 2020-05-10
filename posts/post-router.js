b8795d3d6ec4d8efb1cc09e5cdf0be28db694937
HitzeD <dakotahhitze@gmail.com>
2020-05-10 09:50:49 -0400

    db running with endpoints

--- posts/post-router.js
+++ posts/post-router.js
@@ -5,24 +5,91 @@ const db = require('../data/db-config.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+
+    try {
+
+       const posts = await db.select('*').from('posts');
+
+       res.json(posts);
+
+    } catch (err) {
+
+        next(err);
+    }
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
 
+    try {
+
+        const post = await db('posts').where('id', req.params.id).first();
+
+        res.json(post);
+
+    } catch(err) {
+
+        next(err);
+
+    }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 
+    try {
+
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        };
+
+        const [id] = await db('posts').insert(payload);
+
+        const post = await db('posts').where('id', id).first();
+
+        res.json(post);
+
+    }catch (err) {
+
+        next(err);
+
+    }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
 
+    try {
+
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents
+        }
+
+        await db('posts').where('id', req.params.id).update(payload);
+        const post = await db('posts').where('id', req.params.id).first();
+
+        res.json(post);
+
+    }catch (err) {
+
+        next(err)
+
+    }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
 
+    try {
+
+        await db('posts').where('id', req.params.id).del();
+        res.status(204).end();
+
+    }catch (err) {
+
+        next(err)
+        
+    }
 });
 
 module.exports = router;
